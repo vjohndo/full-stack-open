28646eaaa5e9488e193684c2c0543184c6d3314f
vjohndo <v.john.do@outlook.com>
2023-02-20 16:21:10 +1100

    [Part01] ex 1.2 complete

--- part01/course-information/src/components/Content.js
+++ part01/course-information/src/components/Content.js
@@ -1,15 +1,10 @@
-const Content = (props) => {
-    return <>
-        <p>
-            {props.part1} {props.exercises1}
-        </p>
-        <p>
-            {props.part2} {props.exercises2}
-        </p>
-        <p>
-            {props.part3} {props.exercises3}
-        </p>
-    </>
-}
+import Part from "./Part"
 
+const Content = (props) => {
+    return (<div>
+        <Part part={props.part1} exercises={props.exercises1} />
+        <Part part={props.part2} exercises={props.exercises2} />
+        <Part part={props.part3} exercises={props.exercises3} />
+    </div>)
+}
 export default Content
