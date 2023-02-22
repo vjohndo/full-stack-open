3e3e7ef80ed346ab135029b645ea9eb3bd8fef92
vjohndo <v.john.do@outlook.com>
2023-02-22 16:37:47 +1100

    [Part01] ex 1.8 complete

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -1,17 +1,7 @@
 import { useState } from 'react'
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Statistics = ({good, neutral, bad}) => {
 
-  const handleIncrement = (setter) => {
-    return () => setter((prev) => {
-      return prev + 1
-    })
-  }
-  
   const calculateTotal = () => {
     return good + neutral + bad
   }
@@ -26,16 +16,35 @@ const App = () => {
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <button onClick={handleIncrement(setGood)}>good</button>
-      <button onClick={handleIncrement(setNeutral)}>neutral</button>
-      <button onClick={handleIncrement(setBad)}>bad</button>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
       <p>all {calculateTotal()}</p>
       <p>average {calculateAverage()}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleIncrement = (setter) => {
+    return () => setter((prev) => {
+      return prev + 1
+    })
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleIncrement(setGood)}>good</button>
+      <button onClick={handleIncrement(setNeutral)}>neutral</button>
+      <button onClick={handleIncrement(setBad)}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
