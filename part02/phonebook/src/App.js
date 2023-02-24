4bb6c8abd2361c4ff0075bee5dc12b2fff835368
vjohndo <v.john.do@outlook.com>
2023-02-24 18:58:57 +1100

    [Part02] ex 2.6 completed

---
+++ part02/phonebook/src/App.js
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const newPersonObject = {
+      name: newName
+    }
+    setPersons(persons.concat(newPersonObject))
+    setNewName("")
+  }
+
+  const handleChange = (event) => {
+    const newNameValue = event.target.value
+    setNewName(newNameValue)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, index) => 
+        <p key={index}>
+          {person.name}
+        </p>
+      )}
+    </div>
+  )
+}
+
+export default App
