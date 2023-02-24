7239dff471c5cb1109dc7b7ed9b99d209cacb60e
vjohndo <v.john.do@outlook.com>
2023-02-24 15:59:23 +1100

    [Part02] section a code along

---
+++ part02/code-along/src/App.js
@@ -0,0 +1,14 @@
+import Note from "./components/Note"
+
+const App = ({ notes }) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note=> <Note note={note} />)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
---
+++ part02/code-along/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true
+  },
+  {
+    id: 4,
+    content: 'Another sick line',
+    important: true
+  }
+]
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App notes={notes} />
+)
+
+const result = notes.map(note => note.content)
+console.log(result)
